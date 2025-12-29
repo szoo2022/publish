b8209f905e246e9393d2832839a5ce511fba5d3d
szoo2022 <tempo12.sampo12@gmail.com>
2025-12-29 11:45:07 +0900

    py

--- python_package/sco_log/sco_log.py
+++ python_package/sco_log/sco_log.py
@@ -2,25 +2,27 @@
 # coding: UTF-8
 
 
-# typing.py
-from typing import \
-    Final
+from typing import Final
 
-# logging.py
-from logging import \
-    Logger, \
-    Formatter, \
-    StreamHandler, \
-    DEBUG, \
+from logging import (
+    Logger,
+    Formatter,
+    StreamHandler,
+    FileHandler,
+    DEBUG,
     getLogger
+)
+
+from contextlib import contextmanager
 
 
 LOG_NAME  : Final[str] = "sco_log"
 LOG_LEVEL : Final[int] = DEBUG
-LOG_FORMAT: Final[str] = \
-    "%(asctime)s - %(levelname)8s - %(pathname)s(%(lineno)4d) - " \
-    "%(funcName)s()\n" \
+LOG_FORMAT: Final[str] = (
+    "%(asctime)s - %(levelname)8s - %(pathname)s(%(lineno)4d) - "
+    "%(funcName)s()\n"
     "%(message)s"
+)
 
 
 def sco_log_init() -> None:
@@ -41,4 +43,20 @@ def sco_log_get() -> Logger:
     result: Final[Logger] = getLogger(LOG_NAME)
 
     return result
+
+
+@contextmanager
+def sco_log_tee(log: Logger, s_fpath: str):
+
+    h_file: Final[FileHandler] = FileHandler(s_fpath,
+                                 mode='a', encoding='utf-8')
+    form: Final[Formatter] = Formatter(LOG_FORMAT)
+    h_file.setFormatter(form)
+    log.addHandler(h_file)
+
+    try:
+        yield # Execute with block
+    finally:
+        log.removeHandler(h_file)
+        h_file.close()
 
